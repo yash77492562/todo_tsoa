d52e2a7968c3d85acd4c4b11ac5ba32e72f7115e
yash77492562 <yashyadavpro@gmail.com>
2024-11-04 11:02:29 +0530

    checkPoint

---
+++ src/todos/todosService.ts
@@ -0,0 +1,24 @@
+// src/users/usersController.ts
+import {
+    Body,
+    Controller,
+    Get,
+    Path,
+    Post,
+    Query,
+    Route,
+    SuccessResponse,
+  } from "tsoa";
+  import { Todo } from "./todo";
+  import { TodoService,TodoCreationParams } from "./todoService";
+  
+  @Route("todo")
+  export class TodoController extends Controller {
+    @Get("{todoId}")
+    public async getTodo(
+      @Path() todoId: string
+    ):Promise<Todo> {
+      let todoService = new TodoService();
+      return todoService.get(todoId)
+    }
+  }
